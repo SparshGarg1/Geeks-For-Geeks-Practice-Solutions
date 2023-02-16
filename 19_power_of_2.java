730cfbe50c5641467e36b97be157f54c40533eab
Sparsh Garg <77558006+SparshGarg1@users.noreply.github.com>
2023-02-16 22:07:17 +0530

    Create 19_power_of_2.java

---
+++ 19_power_of_2.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    public static boolean isPowerofTwo(long n)
+    {
+        if(n==0)
+        return false;
+        
+        if(n==1)
+        return true;
+        
+        while(n%2==0)
+        {
+            n=n/2;
+            if(n==1)
+            return true;
+        }
+        return false;
+    }
+    
+}
